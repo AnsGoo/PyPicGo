c1030ae351dee79326775e43f7ab2f2f4230181f
chenghaiwen <haiven_123@163.com>
2021-09-07 14:36:07 +0800

    feat(): 修改了HOME路径获取方式

--- pypicgo/__init__.py
+++ pypicgo/__init__.py
@@ -1,16 +1,8 @@
 import os
-import platform
 from pathlib import Path
 
-system_name = platform.system().lower()
-
-if system_name == 'linux':
-    HOME = os.environ.get('HOME')
-    home = Path(HOME)
-elif system_name == 'windows':
-    HOMEDRIVE = os.environ.get('HOMEDRIVE')
-    HOME = os.environ.get('HOMEPATH')
-    home = Path(HOMEDRIVE).joinpath(HOME)
+home = os.path.expanduser('~')
+HOME = Path(home)
 BASE_DIR = home.joinpath('.PyPicGo')
 if not BASE_DIR.exists():
     BASE_DIR.mkdir(parents=True)
